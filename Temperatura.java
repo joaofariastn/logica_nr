91bf42ee67a8f258d13978e6894fda656d442312
joaofariastn <126178518+joaofariastn@users.noreply.github.com>
2023-03-28 16:56:57 -0300

    Add files via upload

---
+++ Temperatura.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Conversor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // Solicita ao usuário a temperatura em Celsius
+        System.out.print("Digite a temperatura em Celsius: ");
+        double celsius = sc.nextDouble();
+        
+        // Calcula a temperatura em Fahrenheit
+        double fahrenheit = (celsius * 9/5) + 32;
+        
+        // Exibe a temperatura em Fahrenheit
+        System.out.printf("A temperatura em Fahrenheit é: %.2f", fahrenheit);
+        
+        sc.close();
+    }
+}
